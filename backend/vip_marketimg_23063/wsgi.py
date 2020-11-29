c4e57d1e35be09e957acb333a0a49868506ed25e
Crowdbotics <team@crowdbotics.com>
2020-11-29 23:48:39 +0000

    Update models

--- backend/vip_marketimg_23063/wsgi.py
+++ backend/vip_marketimg_23063/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vip_marketimg_23063.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vip_marketimg_23063.settings")
 
 application = get_wsgi_application()
